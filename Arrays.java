99a945575167395342a490821c521fa5b869e144
Muhammad Khan <112765800+khanpopal@users.noreply.github.com>
2022-11-21 22:21:07 -0800

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,128 @@
+public class Arrays
+{
+    private int[] items;
+    private int count;
+
+    public Arrays(int length) {
+        this.items = new int[length];
+    }
+
+    public void insert(int item) {
+        if (items.length == count) {
+            int[] newItems = new int[count * 2];
+            for (int i = 0; i < count; i++)
+                newItems[i] = items[i];
+            items = newItems;
+        }
+        items[count++] = item;
+    }
+
+    public void removeAt(int index) {
+        if (index < 0 || index >= count)
+            throw new IllegalArgumentException("Argument is not valid");
+        for (int i = index; i < count; i++)
+            items[i] = items[i + 1];
+        count--;
+    }
+
+    public void print() {
+        for (int i = 0; i < count; i++) {
+            System.out.println(items[i]);
+        }
+    }
+
+    public void maximum() {
+        int max = items[0];
+        for (int i = 0; i < count; i++) {
+            if (items[i] > max)
+                max = items[i];
+
+        }
+        System.out.println("largest element present in array is " + max);
+    }
+
+    public void minimum() {
+        int min = items[0];
+        for (int i = 0; i > count; i++) {
+            if (items[i] > min)
+                min = items[i];
+
+        }
+        System.out.println("smallest element present in array is " + min);
+    }
+
+    public void product() {
+        int product = items[0];
+        for (int i = 0; i < count; i++) {
+            product = items[i] * product;
+
+        }
+        System.out.println("product of all the numbers are " + product);
+    }
+
+    public void average() {
+        float sum = 0;
+        for (int i = 0; i < count; i++) {
+            sum = sum + items[i];
+
+
+        }
+        float avg = sum / count;
+        System.out.println("average of all the numbers are " + avg);
+    }
+
+    public void reverse() {
+        for (int i = count - 1; i >= 0; i--) {
+            System.out.println(+items[i]);
+        }
+
+
+    }
+
+    public void even() {
+        for (int i = 0; i < count; i++) {
+            if (items[i] % 2 == 0) {
+                System.out.println("even number is " + items[i]);
+            }
+        }
+
+    }
+
+    public void odd() {
+        for (int i = 0; i < count; i++) {
+            if (items[i] % 2 != 0) {
+                System.out.println("odd number is " + items[i]);
+            }
+        }
+
+    }
+
+    public void swap(int i, int j) {
+        int temp = items[i];
+        items[i] = items[j];
+        items[j] = temp;
+
+
+    }
+    public void Prime_Number(){
+
+        for (int i = 0; i <count; i++){
+            boolean prime_flag = false;
+            for (int j = 2; j <=9;j++){
+                if(items[i] != j){
+                    if (items[i] % j ==0){
+                        prime_flag = false;
+                        break;
+                    }
+                    prime_flag = true;
+                }
+            }
+            if (prime_flag){
+                System.out.println("Prime number:"+items[i]+"");
+            }
+        }
+
+    }
+
+
+}
---
+++ main.java
@@ -0,0 +1,15 @@
+public class main
+{
+
+        public static void main(String[] args)
+        {
+            Arrays array = new Arrays(100);
+            SinglyList singlyList = new SinglyList();
+            CircularList circularList = new CircularList();
+            DoublyList doublyList = new DoublyList();
+            StackLinkedList stackLinkedList = new StackLinkedList();
+            StackArray stackArray = new StackArray(10);
+
+        }
+
+}
